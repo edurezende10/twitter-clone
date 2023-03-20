6ba4aa38ff11cb791d5eebda66ff17b8ef3a611c
unknown <edu_rezende11@hotmail.com>
2023-03-19 22:05:21 -0300

    ajustes da homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import twitterLogo from './assets/twitter-logo.svg'
+import{Sparkle} from 'phosphor-react'
 import './components/global.css'
+import Sidebar from './components/Sidebar'
+import { Tweet } from './components/Tweet'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className='Layout'>
-      <aside className='sidebar'>
-        <img className='logo' src={twitterLogo} alt="Logo" />
-        <nav></nav>
-        <button>Tweet</button>
-      </aside>
+     <Sidebar/>
       <div className="content">
-content
+          <main className='timeline'>
+            <div className='timeline-header'>
+              Home
+              <Sparkle/>
+            </div>
+            <form className='new-tweet-form'>
+              <label htmlFor='tweet'>
+                <img src="https://github.com/edurezende10.png" alt="Eduardo Rezende" />
+                <textarea name="" id=""  placeholder="What's happening"  />
+              
+              </label>
+              <button type='submit'>Tweet</button>
+
+            </form>
+            <div className="separator"/>
+            <Tweet/>
+            <Tweet/>
+            <Tweet/>
+            <Tweet/>
+
+          </main>
       </div>
 
     </div>
